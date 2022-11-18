eaaf858f01622cd27e5c4ec36123ef66f7fe14a8
PASINSKI00 <krystian.pasinski00@gmail.com>
2022-11-18 22:56:40 +0100

    removed weight

--- src/app/diet/ingredient.ts
+++ src/app/diet/ingredient.ts
@@ -1,11 +1,9 @@
 export class Ingredient {
     id: number;
     name: string;
-    weight: number;
 
     constructor(id: number, name: string, weight: number) {
         this.id = id;
         this.name = name;
-        this.weight = weight;
     }
 }
