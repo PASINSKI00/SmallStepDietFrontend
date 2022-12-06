c38c9d6ea32a41ac18f5ac5a9ccc4c04d318aafb
PASINSKI00 <krystian.pasinski00@gmail.com>
2022-12-06 21:20:25 +0100

    Created basic styling for finalMeal

--- src/app/final-diet/final-diet.component.ts
+++ src/app/final-diet/final-diet.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { faAdd, faTrashAlt } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-final-diet',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./final-diet.component.sass']
 })
 export class FinalDietComponent implements OnInit {
+  addIcon = faAdd;
+  deleteIcon = faTrashAlt;
 
   constructor() { }
 
