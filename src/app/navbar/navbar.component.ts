04d9e853626924b397ce51da3ce876cc3a7bab7c
PASINSKI00 <krystian.pasinski00@gmail.com>
2023-01-14 21:15:45 +0100

    navbar adapts to whether user is logged in

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 import { Router } from '@angular/router';
+import { SharedService } from '../shared.service';
 
 @Component({
   selector: 'app-navbar',
@@ -9,8 +10,9 @@ import { Router } from '@angular/router';
 export class NavbarComponent implements OnInit {
   @Output() accessEvent = new EventEmitter<string>();
   active: string = '';
+  isLoggedIn: boolean = false;
 
-  constructor(private router: Router) { 
+  constructor(private sharedService: SharedService) { 
     this.active = window.location.pathname.split('/')[1];
     if (this.active == '' || this.active == 'addMeal') {
       this.active = 'home';
@@ -18,6 +20,11 @@ export class NavbarComponent implements OnInit {
   }
 
   ngOnInit(): void {
+    this.sharedService.changeEmitted$.subscribe(
+      (change) => {
+        this.isLoggedIn = change;
+      }
+    );
   }
 
   access() {
