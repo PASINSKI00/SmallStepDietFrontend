94f835fd5b6bfdd1eec5720e9fa41701eca8eff1
PASINSKI00 <krystian.pasinski00@gmail.com>
2023-01-14 15:41:05 +0100

    built groceries view

--- src/app/diet/groceries/groceries.component.ts
+++ src/app/diet/groceries/groceries.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Ingredient } from '../ingredient';
+import { faTrashAlt } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-groceries',
@@ -6,10 +8,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./groceries.component.sass']
 })
 export class GroceriesComponent implements OnInit {
+  deleteIcon = faTrashAlt;
+  ingredients: Array<Ingredient> = [];
 
   constructor() { }
 
   ngOnInit(): void {
+    this.ingredients.push(new Ingredient(1, 'Apple'));
+    this.ingredients.push(new Ingredient(2, 'Banana'));
+    this.ingredients.push(new Ingredient(3, 'Orange'));
+    this.ingredients[0].weight = 230;
+    this.ingredients[1].weight = 320;
+    this.ingredients[2].weight = 420;
   }
 
 }
