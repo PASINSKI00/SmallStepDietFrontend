ebaa5beead3fdad92c830d91a70eb815665fe8a9
PASINSKI00 <krystian.pasinski00@gmail.com>
2023-01-16 15:13:31 +0100

    added account routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,6 +7,11 @@ import { AddMealComponent } from './add-meal/add-meal.component';
 import { FinalDietComponent } from './diet/final-diet/final-diet.component';
 import { GroceriesComponent } from './diet/groceries/groceries.component';
 import { FinishedComponent } from './diet/finished/finished.component';
+import { AccountHomeComponent } from './account/account-home/account-home.component';
+import { UserInfoComponent } from './account/user-info/user-info.component';
+import { DietHistoryComponent } from './account/diet-history/diet-history.component';
+import { BodyInfoComponent } from './account/body-info/body-info.component';
+import { ReviewComponent } from './account/review/review.component';
 
 const routes: Routes = [
   {
@@ -41,6 +46,28 @@ const routes: Routes = [
   {
     path: 'diet/finished',
     component: FinishedComponent,
+  },
+  {
+    path: 'account',
+    component: AccountHomeComponent,
+    children: [
+      {
+        path: 'user',
+        component: UserInfoComponent,
+      },
+      {
+        path: 'diets',
+        component: DietHistoryComponent,
+      },
+      {
+        path: 'bodyinfo',
+        component: BodyInfoComponent,
+      },
+      {
+        path: 'reviews',
+        component: ReviewComponent,
+      },
+    ],
   },
 ];
 
