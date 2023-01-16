2b1dbf5d698e27ad7ac9cac7ab407fe8e9b0d40e
PASINSKI00 <krystian.pasinski00@gmail.com>
2023-01-16 15:14:20 +0100

    implemented a functioning side-bar

---
+++ src/app/account/account-home/account-home.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { faUser, faStar } from '@fortawesome/free-regular-svg-icons';
+import { faCalculator, faBook } from '@fortawesome/free-solid-svg-icons';
+
+@Component({
+  selector: 'app-account-home',
+  templateUrl: './account-home.component.html',
+  styleUrls: ['./account-home.component.sass']
+})
+export class AccountHomeComponent implements OnInit {
+  userIcon = faUser;
+  dietsIcon = faBook;
+  bodyInfoIcon = faCalculator;
+  reviewsIcon = faStar;
+  activeLink = '';
+
+  constructor() {
+    this.checkActiveLink();
+  }
+
+  ngOnInit(): void {
+  }
+
+  checkActiveLink() {
+    this.activeLink = window.location.pathname.split('/')[2];
+    console.log(this.activeLink);
+  }
+}
