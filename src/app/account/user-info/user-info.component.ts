c2f68525fcd2444e89ea18b25ab77b389d5e9949
PASINSKI00 <krystian.pasinski00@gmail.com>
2023-01-17 18:06:33 +0100

    account/user created with logout button

--- src/app/account/user-info/user-info.component.ts
+++ src/app/account/user-info/user-info.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { lastValueFrom } from 'rxjs';
 import { SharedService } from 'src/app/shared.service';
+import { User } from 'src/app/user';
+import { UserService } from 'src/app/user.service';
 
 @Component({
   selector: 'app-user-info',
@@ -7,13 +11,19 @@ import { SharedService } from 'src/app/shared.service';
   styleUrls: ['./user-info.component.sass']
 })
 export class UserInfoComponent implements OnInit {
+  user: User = null!;
 
-  constructor(private sharedService: SharedService) { }
+  constructor(private sharedService: SharedService, private userService: UserService, private router: Router) { }
 
   ngOnInit(): void {
+    this.userService.getMe().subscribe(response => {
+      this.user = JSON.parse(response.body);
+      console.log(this.user);
+    });
   }
 
   logout() {
     this.sharedService.logout();
+    this.router.navigate(['/home']);
   }
 }
