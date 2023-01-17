fba568d5b79224a99c2f4110831fae920222d2d8
PASINSKI00 <krystian.pasinski00@gmail.com>
2023-01-17 18:05:28 +0100

    added user class

---
+++ src/app/user.ts
@@ -0,0 +1,9 @@
+export class User {
+    imageUrl: string;
+    name: string;
+
+    constructor(imageUrl: string, name: string) {
+        this.imageUrl = imageUrl;
+        this.name = name;
+    }
+}
