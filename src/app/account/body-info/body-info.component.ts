f33892854d0492bc36968d69a33ee13569f6f9b3
PASINSKI00 <krystian.pasinski00@gmail.com>
2023-01-19 19:26:22 +0100

    bodyinfo form implemented

--- src/app/account/body-info/body-info.component.ts
+++ src/app/account/body-info/body-info.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, Validators } from '@angular/forms';
+import { BodyInfoService } from './body-info.service';
 
 @Component({
   selector: 'app-body-info',
@@ -7,9 +9,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BodyInfoComponent implements OnInit {
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder, private bodyInfoService: BodyInfoService) { }
 
   ngOnInit(): void {
+    this.bodyInfoService.getBodyInfo().subscribe(response => {
+      this.bodyInfoForm.patchValue(JSON.parse(response.body));
+    });
   }
+
+  bodyInfoForm = this.formBuilder.group({
+    goal: 'LOSE_WEIGHT',
+    height: new FormControl('', [Validators.required]),
+    weight: new FormControl('', [Validators.required]),
+    age: new FormControl('', [Validators.required]),
+    pal: new FormControl('', [Validators.required, Validators.min(1.4), Validators.max(2.4)]),
+  });
+
+  onSubmit() {
+    this.bodyInfoService.saveBodyInfo(this.bodyInfoForm).subscribe(response => {
+      if(response.status != 201) {
+        alert('Error');
+        console.log(response);
+      }
+    });
+  }
+
 
 }
