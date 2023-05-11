e1f6e2ac86aae723cdd9e150c1adbd0b2392550f
PASINSKI00 <krystian.pasinski00@gmail.com>
2023-05-11 23:18:40 +0200

    Implemented adding users image

--- src/app/account/user-info/user-info.component.ts
+++ src/app/account/user-info/user-info.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { ImageCroppedEvent } from 'ngx-image-cropper';
 import { lastValueFrom } from 'rxjs';
+import { ImageService } from 'src/app/image.service';
 import { SharedService } from 'src/app/shared.service';
 import { User } from 'src/app/user';
 import { UserService } from 'src/app/user.service';
@@ -12,18 +14,45 @@ import { UserService } from 'src/app/user.service';
 })
 export class UserInfoComponent implements OnInit {
   user: User = null!;
+  imageChangedEvent: any = '';
+  croppedImage: any = '';
+  hideFileInput = true;
 
-  constructor(private sharedService: SharedService, private userService: UserService, private router: Router) { }
+  constructor(private sharedService: SharedService, private userService: UserService, private router: Router, private imageService: ImageService) { }
 
   ngOnInit(): void {
     this.userService.getMe().subscribe(response => {
       this.user = JSON.parse(response.body);
-      console.log(this.user);
     });
   }
 
   logout() {
     this.sharedService.logout() ? this.sharedService.logout() : null;
     this.router.navigate(['/home']);
+  }
+
+  fileChangeEvent(event: any): void {
+    this.imageChangedEvent = event;
+  }
+
+  imageCropped(event: ImageCroppedEvent) {
+    this.croppedImage = event.base64;
+  }
+
+  loadImageFailed() {
+    alert("Image load failed. Please try again.");
+  }
+
+  uploadImage() {
+    this.imageService.uploadUserImage(this.croppedImage).subscribe(
+      (response) => {
+        alert("Image uploaded successfully!");
+        this.hideFileInput = true;
+        location.reload();
+      },
+      (error) => {
+        alert("Image upload failed. Please try again.");
+      }
+    );
   }
 }
