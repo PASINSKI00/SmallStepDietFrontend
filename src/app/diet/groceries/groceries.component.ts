17a5f74e98aad54ba4152edcfa623512e11e73b3
PASINSKI00 <krystian.pasinski00@gmail.com>
2023-09-23 14:26:06 +0200

    Shopping list view fixed

--- src/app/diet/groceries/groceries.component.ts
+++ src/app/diet/groceries/groceries.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit } from '@angular/core';
 import { Ingredient } from '../ingredient';
 import { faTrashAlt } from '@fortawesome/free-solid-svg-icons';
 import { DietService } from '../diet.service';
@@ -11,22 +11,42 @@ import { lastValueFrom } from 'rxjs';
 })
 export class GroceriesComponent implements OnInit {
   deleteIcon = faTrashAlt;
-  ingredients: Array<Ingredient> = [];
+  ingredients: Array<Array<Ingredient>> = [];
+  numOfColumns: number = 3;
+  elementesPerColumn: number = 0;
 
-  constructor(private dietService: DietService) { }
+  constructor(private dietService: DietService, private elRef: ElementRef) { }
 
   ngOnInit(): void {
     this.getGroceries();
   }
 
   async getGroceries() {
-    const response$ = this.dietService.getGroceries();
-    const lastValue$ = await lastValueFrom(response$);
-    if(lastValue$.status != 200) {
+    const observer$ = this.dietService.getGroceries();
+    const lastValue$ = await lastValueFrom(observer$).catch(error => {
       alert("Something went wrong");
+      console.log(error);
+    });
+
+    if(lastValue$ == undefined) {
       return;
     }
 
-    this.ingredients = JSON.parse(lastValue$.body);
+    let allIngredients: Array<Ingredient> = JSON.parse(lastValue$.body);
+
+    if(allIngredients.length >= 6) {
+      this.numOfColumns = 3;
+    } else if(allIngredients.length >= 4) {
+      this.numOfColumns = 2;
+    } else {
+      this.numOfColumns = 1;
+    }
+
+    this.elRef.nativeElement.style.setProperty('--num-of-columns', this.numOfColumns);
+    this.elementesPerColumn = Math.ceil(allIngredients.length / this.numOfColumns);
+
+    for(let i = 0; i < this.numOfColumns; i++) {
+      this.ingredients.push(allIngredients.slice(i * this.elementesPerColumn, (i + 1) * this.elementesPerColumn));
+    }
   }
 }
