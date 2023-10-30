125807e2b43568d7f59cbf1ed4d753da8d84042e
PASINSKI00 <krystian.pasinski00@gmail.com>
2023-10-30 17:18:55 +0100

    improved & moved overlays displaying logic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,7 @@
-import { Component, Injector } from '@angular/core';
-import { Overlay, OverlayRef } from "@angular/cdk/overlay";
-import { ComponentPortal } from "@angular/cdk/portal";
+import { Component } from '@angular/core';
 import { LoginComponent } from "./overlays/login/login.component";
 import { SignupComponent } from "./overlays/signup/signup.component";
 import { SharedService } from './shared.service';
-import { Router } from '@angular/router';
 import { RedirectDetails } from './overlays/redirect/redirect-details';
 import { RedirectComponent } from './overlays/redirect/redirect.component';
 import { AlertDetails } from './overlays/alert/alert-details';
@@ -17,65 +14,19 @@ import { AlertComponent } from './overlays/alert/alert.component';
 })
 export class AppComponent {
   title = 'frontend';
-  overlayRef!: OverlayRef;
 
-  constructor(private overlay: Overlay, private _sharedService: SharedService, private router: Router, private injector: Injector) {
-    _sharedService.changeEmitted$.subscribe( change => {
+  constructor(private sharedService: SharedService) {
+    sharedService.changeEmitted$.subscribe( change => {
       if(change == 'closeOverlay')
-        this.closeOverlay();
+        this.sharedService.closeOverlay();
       else if(change == 'loginOverlay')
-        this.loginOverlay();
+        this.sharedService.showOverlay(LoginComponent)
       else if(change == 'signupOverlay')
-        this.signupOverlay();
+        this.sharedService.showOverlay(SignupComponent)
       else if(change instanceof RedirectDetails)
-        this.redirectOverlay(change);
+        this.sharedService.showOverlay(RedirectComponent, change, RedirectDetails)
       else if(change instanceof AlertDetails)
-        this.alertOverlay(change);
+        this.sharedService.showOverlay(AlertComponent, change, AlertDetails)
       });
    }
-
-  loginOverlay(){
-    this.closeOverlay();
-    this.overlayRef = this.overlay.create();
-    const componentPortal = new ComponentPortal(LoginComponent);
-    this.overlayRef.attach(componentPortal);
-  }
-
-  signupOverlay(){
-    this.closeOverlay();
-    this.overlayRef = this.overlay.create();
-    const componentPortal = new ComponentPortal(SignupComponent);
-    this.overlayRef.attach(componentPortal);
-  }
-
-  redirectOverlay(details: RedirectDetails){
-    this.closeOverlay();
-    this.overlayRef = this.overlay.create();
-    const injectorWithRedirectData = Injector.create({
-      providers: [
-        { provide: RedirectDetails, useValue: details },
-      ],
-      parent: this.injector,
-    });
-    const componentPortal = new ComponentPortal(RedirectComponent, null, injectorWithRedirectData);
-    this.overlayRef.attach(componentPortal);
-  }
-
-  alertOverlay(details: AlertDetails){
-    this.closeOverlay();
-    this.overlayRef = this.overlay.create();
-    const injectorWithAlertData = Injector.create({
-      providers: [
-        { provide: AlertDetails, useValue: details },
-      ],
-      parent: this.injector,
-    });
-    const componentPortal = new ComponentPortal(AlertComponent, null, injectorWithAlertData);
-    this.overlayRef.attach(componentPortal);
-  }
-
-  closeOverlay(){
-    if(this.overlayRef)
-      this.overlayRef.dispose();
-  }
 }
--- src/app/shared.service.ts
+++ src/app/shared.service.ts
@@ -1,17 +1,23 @@
-import { Injectable } from '@angular/core';
+import { Injectable, Injector } from '@angular/core';
 import { Subject } from 'rxjs';
 import { CookieService } from 'ngx-cookie-service';
 import { Meal } from './diet/meal';
 import { AlertDetails } from './overlays/alert/alert-details';
+import { Overlay, OverlayRef } from '@angular/cdk/overlay';
+import { ComponentPortal, ComponentType } from '@angular/cdk/portal';
+import { BaseComponent } from './overlays/base/base.component';
+import { BaseDetails } from './overlays/base/base-details';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SharedService {
+  shouldShowOVerlay: boolean = false;
   private emitChangeSource = new Subject<any>();
   changeEmitted$ = this.emitChangeSource.asObservable();
+  overlayRef!: OverlayRef;
 
-  constructor(private cookieService: CookieService) { }
+  constructor(private cookieService: CookieService, private overlay: Overlay, private injector: Injector) { }
 
   emitChange(change: any) {
     this.emitChangeSource.next(change);
@@ -71,5 +77,42 @@ export class SharedService {
   clearActiveDiet() {
     localStorage.removeItem('activeDiet');
     this.cookieService.delete('activeDietId');
+  }
+
+  closeOverlay(){
+    if(this.overlayRef){
+      this.shouldShowOVerlay = false;
+      setTimeout(() => { this.overlayRef.dispose(); }, 200);
+    }
+  }
+
+  showOverlay(component: ComponentType<BaseComponent>, details?: BaseDetails, detailsType?: ComponentType<BaseDetails>){
+    let componentPortal;
+    let injectorWithAlertData;
+
+    if(this.overlayRef){
+      this.overlayRef.dispose();
+    }
+
+    if(details && !detailsType){
+      throw new Error('You need to provide details together with detailsType');
+    }
+
+    this.shouldShowOVerlay = true;
+    this.overlayRef = this.overlay.create();
+
+    if(details){
+      injectorWithAlertData = Injector.create({
+        providers: [
+          { provide: detailsType, useValue: details },
+        ],
+        parent: this.injector,
+      });
+      componentPortal = new ComponentPortal(component, null, injectorWithAlertData);
+    } else {
+      componentPortal = new ComponentPortal(component);
+    }
+
+    this.overlayRef.attach(componentPortal);
   }
 }
