9e60a3b54feea1771ecaa12fc8deb6f45119d33e
PASINSKI00 <krystian.pasinski00@gmail.com>
2023-11-02 13:33:35 +0100

    reading animation from file

--- src/app/overlays/base/base.component.ts
+++ src/app/overlays/base/base.component.ts
@@ -1,21 +1,13 @@
-import { animate, state, style, transition, trigger } from '@angular/animations';
 import { Component, OnInit } from '@angular/core';
 import { SharedService } from 'src/app/shared.service';
+import { fadeSlideInOut } from 'src/app/animations';
 
 @Component({
   selector: 'app-base',
   templateUrl: './base.component.html',
   styleUrls: ['./base.component.sass'],
   animations: [
-    trigger('fadeSlideInOut', [
-      transition(':enter', [
-        style({ opacity: 0, transform: 'translateY(10px)' }),
-        animate('400ms', style({ opacity: 1, transform: 'translateY(0)' })),
-      ]),
-      transition(':leave', [
-        animate('200ms', style({ opacity: 0, transform: 'translateY(10px)' })),
-      ]),
-    ])
+    fadeSlideInOut
   ]
 })
 export class BaseComponent implements OnInit {  
