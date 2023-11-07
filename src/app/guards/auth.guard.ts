4d486f27f0c7222b5762a7a033db5b215c340f45
PASINSKI00 <krystian.pasinski00@gmail.com>
2023-11-07 23:11:06 +0100

    Alert on not authorized

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -1,8 +1,16 @@
 import { CanActivateFn } from '@angular/router';
 import { SharedService } from '../shared.service';
 import { inject } from '@angular/core';
+import { AlertDetails } from '../overlays/alert/alert-details';
 
 export const authGuard: CanActivateFn = (route, state) => {
   const sharedService = inject(SharedService)
-  return sharedService.isLoggedIn();
+
+  if(sharedService.isLoggedIn()) {
+    return true;
+  }
+
+  const alert = new AlertDetails("You need to be logged in to access this part of the website");
+  sharedService.emitChange(alert);
+  return false;
 };
